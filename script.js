d880384e2a6742a6c76788ead088a83504ffeb03
WASHIUL ISLAM <washiul92@gmail.com>
2022-10-26 18:46:50 +0530

    Added web counter

---
+++ script.js
@@ -0,0 +1,21 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+
+
+function increment() {
+    count += 1 
+    countEl.innerText = count
+}
+
+function save(){
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+
+}
+function reset(){
+    count = 0
+    countEl.innerText = count
+}
+
